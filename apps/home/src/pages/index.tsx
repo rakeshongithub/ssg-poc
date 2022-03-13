85309cd1e1ba5498162d66511166406ff6b5abb9
Rakesh Kumar <rakeshpersonal@gmail.com>
2022-03-13 11:24:06 +0530

    feat: implemented CSR

--- apps/home/src/pages/index.tsx
+++ apps/home/src/pages/index.tsx
@@ -1,67 +1,20 @@
 import React from 'react';
-import Image from 'next/image';
-import Button from '../components/atoms/Button';
+import Posts from '../components/organisms/Posts';
 
 export default function Home() {
   return (
     <div className="container" data-testid="HomeContainer">
       <div>
-        <title>Create Next App</title>
+        <title>SSG POC</title>
         <link rel="icon" href="/favicon.ico" />
       </div>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          SSG POC - POSTS
         </h1>
-        <Button>Click Here</Button>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/zeit/next.js/tree/master/examples" className="card">
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        <Posts />
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            width="100px"
-            height="15px"
-            className="logo"
-          />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -69,8 +22,6 @@ export default function Home() {
           padding: 0 0.5rem;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
         }
 
         main {
@@ -78,8 +29,6 @@ export default function Home() {
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
         }
 
         footer {
